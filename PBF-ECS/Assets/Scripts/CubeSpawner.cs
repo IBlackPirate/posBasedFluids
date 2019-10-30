87311d4897c6f35825ac4a78ca83ef8a489cc673
blackpirate <mr.blackpirate@yandex.ru>
2019-10-30 13:07:24 +0300

    initial project

---
+++ PBF-ECS/Assets/Scripts/CubeSpawner.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace Assets.Scripts
+{
+    public class CubeSpawner : MonoBehaviour, ISpawner
+    {
+        public int FluidCount => HeightCount * WidthCount * LenghtCount;
+
+        [SerializeField] private int HeightCount;
+        [SerializeField] private int WidthCount;
+        [SerializeField] private int LenghtCount;
+        [SerializeField] private float Radius = .5f;
+
+        private float Diameter { get { return 2 * Radius; } }
+
+        public Vector3[] Spawn()
+        {
+            Vector3[] result = new Vector3[FluidCount];
+            for (int i = 0; i < WidthCount; i++)
+                for (int j = 0; j < HeightCount; j++)
+                    for (int k = 0; k < LenghtCount; k++)
+                        result[i] = new Vector3(i * Diameter, j * Diameter, k * Diameter);
+            return result;
+        }
+    }
+}
---
+++ PBF-ECS/Assets/Scripts/WaterInitializer.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+using Unity.Rendering;
+using Unity.Transforms;
+using Unity.Collections;
+
+namespace Assets.Scripts
+{
+    public class WaterInitializer : MonoBehaviour
+    {
+        [SerializeField] private Mesh fluidMesh;
+        [SerializeField] private Material fluidMaterial;
+
+        [SerializeField] private ISpawner spawner;
+
+        private EntityManager activeEntityManager;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            Initialize();
+            activeEntityManager = World.Active.EntityManager;
+        }
+
+        void Initialize()
+        {
+            var fluidArchetype = activeEntityManager.CreateArchetype(
+                typeof(Translation),
+                typeof(RenderMesh),
+                typeof(LocalToWorld)
+                );
+
+            var fluids = new NativeArray<Entity>(spawner.FluidCount, Allocator.Temp);
+            activeEntityManager.CreateEntity(fluidArchetype, fluids);
+
+            for(int i = 0; i<spawner.FluidCount; i++)
+            {
+
+            }
+
+            fluids.Dispose();
+        }
+    }
+}
